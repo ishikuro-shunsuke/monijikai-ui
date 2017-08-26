869b14a524320fba89e3f3aae7cf20d3665f27e4
Shunsuke Ishikuro <ishikuro.shunsuke@gmail.com>
2017-08-26 10:02:14 +0900

    画面遷移書いた

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import {List, ListItem} from 'material-ui/List';
 import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import io from 'socket.io-client';
+
+const modes = {
+  TOP: 'TOP',
+  LIST: 'LIST',
+  RESULT: 'RESULT',
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      flag: true,
+      mode: modes.TOP,
+      name: '',
+      phone: '',
+      numOfPeople: 4,
+      delay: 15,
       socket: null,
       candidates: [
         {
@@ -62,22 +74,46 @@ class App extends Component {
     this.setState({ candidates });
   }
 
+  onHandleSearch() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+    });
+    this.setState({ mode: modes.LIST });
+  }
+
+  onHandleReserve() {
+    this.setState({ mode: modes.RESULT });
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <div>
-          { (this.state.flag) ?
-          <List>
-          {this.state.candidates.map((value, index) =>
-            <ListItem key={index}>
-              <FlatButton fullWidth style={{ height: '100%' }} onClick={this.onHandleClick.bind(this)}>
-                <img style={{ display: 'block', objectFit: 'contain', width: '100%'}} src={process.env.PUBLIC_URL + `/assets/${value.img}.png`} />
-              </FlatButton>
-              <p>{value.votes} voted!</p>
-            </ListItem>
-          )}
-          </List>
-          : <p>nothing</p> }
+          { (this.state.mode === modes.TOP) ?
+            <div>
+              <h1>MONIJIKAI</h1>
+              名前(カナ): <TextField /><br />
+              人数: <TextField /><br />
+              <RaisedButton label="探す" onClick={this.onHandleSearch.bind(this)}/>
+            </div>
+          : (this.state.mode === modes.LIST) ?
+            <div>
+              <List>
+              {this.state.candidates.map((value, index) =>
+                <ListItem key={index}>
+                  <FlatButton fullWidth style={{ height: '100%' }} onClick={this.onHandleClick.bind(this)}>
+                    <img style={{ display: 'block', objectFit: 'contain', width: '100%'}} src={process.env.PUBLIC_URL + `/assets/${value.img}.png`} />
+                  </FlatButton>
+                  <p>{value.votes} voted!</p>
+                </ListItem>
+              )}
+              </List>
+              <RaisedButton label="予約する" onClick={this.onHandleReserve.bind(this)}/>
+            </div>
+          :
+            <p>予約できました</p>
+          }
         </div>
       </MuiThemeProvider>
     );
